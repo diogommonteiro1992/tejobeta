82dd1ddd113a85f8048f1f643b118d006feb86e1
diogommonteiro1992 <diogomonteiro1992@icloud.com>
2025-07-27 19:55:38 +0000

    deleted:    README.md  	deleted:    client_schema.sql  	deleted:    cliente001/.config.php  	deleted:    cliente001/admin/dashboard.php  	deleted:    cliente001/admin/index.php  	deleted:    cliente001/admin/templates/dashboard.twig  	deleted:    cliente001/admin/templates/login.twig  	deleted:    cliente001/includes/twig_loader.php  	deleted:    cliente001/index.php  	deleted:    cliente001/modules/carousel.twig  	deleted:    cliente001/templates/default/page.twig  	deleted:    root/admin/dashboard.php  	deleted:    root/admin/login.php  	deleted:    root/admin/templates/dashboard.twig  	deleted:    root/admin/templates/login.twig  	deleted:    root/api/validador.php  	deleted:    root/config.php  	deleted:    root/includes/twig_loader.php  	deleted:    root/index.php  	deleted:    root/public/templates/footer.twig  	deleted:    root/public/templates/header.twig  	deleted:    root/public/templates/home.twig

--- root/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/config.php';
-require_once __DIR__ . '/includes/twig_loader.php';
-
-$twig = get_twig(PUBLIC_PATH . '/templates');
-
-echo $twig->render('home.twig');
